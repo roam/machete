f2e6630185849b40c85d72fdcf2583115c6dfcb5
Kevin Wetzels <kevin@roam.be>
2014-08-07 21:06:13 +0200

    Add UTC datetime filter

--- blogger/machete/filters.py
+++ blogger/machete/filters.py
@@ -2,6 +2,12 @@
 from __future__ import (unicode_literals, print_function, division,
                         absolute_import)
 
+from datetime import datetime
+
+from django import forms
+from django.conf import settings
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 import django_filters
 
@@ -25,3 +31,26 @@ class SearchFilter(QuerySetMethodFilter):
     def __init__(self, *args, **kwargs):
         kwargs['method'] = 'search'
         super(SearchFilter, self).__init__(*args, **kwargs)
+
+
+class UtcDateTimeField(forms.DateTimeField):
+
+    def to_python(self, value):
+        if not value:
+            return None
+        if isinstance(value, datetime):
+            return value
+        try:
+            if value.endswith('Z'):
+                value = value[:-1]
+            o = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
+            o = timezone.make_aware(o, timezone.utc)
+            if not getattr(settings, 'USE_TZ', False):
+                o = timezone.make_naive(o, timezone.get_default_timezone())
+            return o
+        except ValueError:
+            raise ValidationError(self.error_messages['invalid'])
+
+
+class UtcDateTimeFilter(django_filters.Filter):
+    field_class = UtcDateTimeField
