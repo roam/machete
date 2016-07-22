5b5ffba18f2492e128fd90a09de3c4b721814928
Kevin Wetzels <kevin@roam.be>
2016-07-22 09:29:39 +0200

    Up version number to 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='machete-jsonapi',
-    version='0.2.5',
+    version='0.3.0',
     author='Kevin Wetzels',
     author_email='kevin@roam.be',
     url='https://github.com/roam/machete',
@@ -13,7 +13,7 @@ setup(
     license='BSD',
     description='Django-based JSON API library',
     long_description=open('README.rst').read(),
-    classifiers = [
+    classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
